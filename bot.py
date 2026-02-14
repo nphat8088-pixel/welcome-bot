9c77fa912aa913ef248b8f63b5b30bfc4b6f8e93
nphat8088-pixel <nphat8088@gmail.com>
2026-02-14 18:36:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,25 @@ import discord
 from discord.ext import commands
 import os
 
+TOKEN = os.getenv("TOKEN")
+WELCOME_CHANNEL_ID = int(os.getenv("WELCOME_CHANNEL_ID"))
+
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
-    print(f"Bot đã online: {bot.user}")
+    print(f"Bot online: {bot.user}")
+
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(1472154096566800477)  # thay ID kênh welcome
+    channel = bot.get_channel(WELCOME_CHANNEL_ID)
     if channel:
-        await channel.send(f"Chào mừng {member.mention} vào server 🎉")
+        await channel.send(f"👋 Chào mừng {member.mention} đến với server!")
 
 
-bot.run(os.getenv("TOKEN"))
+bot.run(TOKEN)
